067732041eefde6652d14e5fec575b61490618e1
Ivan Trunaev <trunaev.ivan@icloud.com>
2023-12-05 10:02:15 +0500

    Config fix

--- lib/Anarchy.pm
+++ lib/Anarchy.pm
@@ -5,7 +5,7 @@ use Mojo::Base 'Mojolicious', -signatures;
 sub startup ($self) {
 
   # Load configuration from config file
-  my $config = $self->plugin('NotYAMLConfig');
+  my $config = $self->plugin('Config');
 
   # Configure the application
   $self->secrets($config->{secrets});
