9f3a06a1d0ce14d29d24d4d0a1d7d60499600a12
Ivan Trunaev <trunaev.ivan@icloud.com>
2024-08-22 00:42:44 +0500

    Add translate url

--- lib/Anarchy.pm
+++ lib/Anarchy.pm
@@ -16,6 +16,8 @@ sub startup {
 	my $config = $self->{config} = new Anarchy::Config;
 	$self->secrets($config->{secrets});
 	
+	$self->init_helpers();
+	
 	my $r = $self->routes;
 	$r->get('/')->to('News#news');
 	$r->get('/feed.rss')->to('News#news');
@@ -29,6 +31,20 @@ sub startup {
 	$r->get('/:section/*article')->to('News#article');
 
 	$r->get('/merch')->to('Merch#merch');
+}
+
+sub init_helpers {
+	my $self = shift;
+
+    $self->helper(tr_url => sub { 
+        my ($c) = @_;
+        my $r = $c->url_for;
+        my $p = $c->req->params();
+        $r .= '?1=1';
+        $r .= '&' . $p if $p;
+        $r .= '&_x_tr_sl=ru&_x_tr_tl=en&_x_tr_hl=ru&_x_tr_pto=wapp';
+        return $r;
+    });
 }
 
 
