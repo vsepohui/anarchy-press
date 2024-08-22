d89c8039abc03a495d84172173ea69a13985ab7e
Ivan Trunaev <trunaev.ivan@icloud.com>
2024-08-23 03:31:58 +0500

    fix

--- public/css/make-anarhcy-css.sh
+++ public/css/make-anarhcy-css.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 
-cat bootstrap.css style.css > anarchy.css
-java -jar ../../tools/yuicompressor-2.4.8.jar jquery.emojipicker.css jquery.emojipicker.a.css anarchy.css > anarchy.min.css
+cat bootstrap.css jquery.emojipicker.a.css style.css > anarchy.css
+java -jar ../../tools/yuicompressor-2.4.8.jar anarchy.css > anarchy.min.css
