01a805c7b4f96a05cbfe09244ab901e1e397bfc3
Ivan Trunaev <trunaev.ivan@icloud.com>
2024-08-22 07:23:16 +0500

    add

---
+++ public/video/make-video-from-parts.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+cat Buddha\ Maytreya\ -\ Karma\ and\ Dharma\ Teaching,\ introduction\ to\ practice\!.mp4.gz.part*>'Buddha Maytreya - Karma and Dharma Teaching, introduction to practice!.mp4.gz'
+gzip -d *.gz
