0b9fe03d596e5cf3a491b59bf3fff3f5caf92a04
Ivan Trunaev <trunaev.ivan@icloud.com>
2024-08-23 03:38:43 +0500

    fix

--- public/js/make-anarhcy-js.sh
+++ public/js/make-anarhcy-js.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 
-cat bootstrap.bundle.min.js jquery-2.2.4.min.js imagesloaded.pkgd.min.js masonry.pkgd.min.js script.js > anarchy.js
+cat bootstrap.bundle.min.js jquery-1.12.4.min.js imagesloaded.pkgd.min.js masonry.pkgd.min.js script.js > anarchy.js
 ../../tools/UglifyJS/bin/uglifyjs anarchy.js > anarchy.min.js
